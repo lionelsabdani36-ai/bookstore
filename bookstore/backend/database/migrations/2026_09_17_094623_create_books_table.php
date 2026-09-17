e9d90ec65522251dbb19cc4dbd548418f8069aed
lionelsabdani36-ai <lionelsabdani36@gmail.com>
2026-09-17 16:55:43 +0700

    update backend

---
+++ bookstore/backend/database/migrations/2026_09_17_094623_create_books_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('release_date')->nullable();
+            $table->integer('stock')->default(0);
+            $table->integer('cost_price')->default(0);
+            $table->integer('selling_price')->default(0);
+            $table->string('category')->nullable();
+            $table->integer('profit')->default(0);
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
